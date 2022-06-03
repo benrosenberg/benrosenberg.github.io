ea9085c73968065b0e174568550b624389c3ecd1
Ben Rosenberg <xethrog42@gmail.com>
2022-06-03 14:23:47 -0400

    make update.sh (by way of replace_body.py) also update header for viewport width

--- posts/replace_body.py
+++ posts/replace_body.py
@@ -1,3 +1,5 @@
+import re
+
 with open('index.html', 'r') as f:
 
     index = ''.join(f.readlines())
@@ -6,6 +8,12 @@ with open('sidebar.html', 'r') as s:
 
     sidebar = ''.join(s.readlines())
 
-print(index.replace('Directory Tree', 'Posts')
+viewport_str = 'head>\n\t<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">\n\t<'
+
+out =(index.replace('Directory Tree', 'Posts')
            .replace('<body>', sidebar)
            .replace('/"', '/index.html"'))
+
+out = re.sub('head>[^<]*<', viewport_str, out, 1)
+
+print(out)
